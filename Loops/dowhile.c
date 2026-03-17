c40086b2f5c5782d38cf7f9c1b6d8d3c2ed948e6
Dave Shashwat <daveshashwat46@gmail.com>
2026-03-17 21:45:32 +0530

    loops

---
+++ Loops/dowhile.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int i=1;
+    do{
+        printf("\t%d",i);
+        i+=1;
+    }while(i<=10);
+    return 0;
+}
---
+++ Loops/while.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a=1;
+    while(a <=10){
+        printf("\n%d",a);
+        a+=1;
+    }
+    return 0;
+}
