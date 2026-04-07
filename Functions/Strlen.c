c640222da1dbbd86c619834cb0fedb5eb9c9fdd4
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-07 12:41:10 +0530

    ..

---
+++ Functions/Strlen.c
@@ -0,0 +1,20 @@
+//WAP to find str and return length of the string 
+#include <stdio.h>
+int main(){
+    char str[100];
+    int i=0;
+    printf("\nEnter the string:- ");
+    gets(str);
+    while (str[i] != '\0'){
+        i++;
+    }
+    printf("\nLength of the string is:- %d",i);
+    return 0;
+}
+int stLen(char str[]){
+    int i=0;
+    while (str[i] != '\0'){
+        i++;
+    }
+    return i;
+}
