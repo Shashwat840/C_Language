3fbe7d0fcf6769b0e72aa3cd29980cf14c53d972
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-09 09:45:11 +0530

    ..

--- Functions/fibonacii.c
+++ Functions/fibonacii.c
@@ -9,15 +9,17 @@ int main(){
     return 0;
 }
 void Fibonacii(int n){
-    int a=0,b=1,ans;
+    int a=0,b=1,ans,sum = 0;
     for (int i = 0; i <= n; i++)
     {
         ans = a+b;
         a=b;
         b=ans;
         printf("\t%d",ans);
+        sum = sum + ans;
         // ans=a;
         // Fibonacii(n-1);
     }
     // printf("\nLast element:- %d",ans);
+    printf("\nSum of fibonacii upto last element is:- %d",sum);
 }
