db49010bfa381cf78921a6f15ebe5b18c0b36ff6
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-09 09:42:59 +0530

    ..

---
+++ Functions/fibonacii.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void Fibonacii(int n);
+int main(){
+    int no;
+    printf("\nEnter the number upto where you want to go :- ");
+    scanf("%d",&no);
+    printf("\n0\t1");
+    Fibonacii(no);
+    return 0;
+}
+void Fibonacii(int n){
+    int a=0,b=1,ans;
+    for (int i = 0; i <= n; i++)
+    {
+        ans = a+b;
+        a=b;
+        b=ans;
+        printf("\t%d",ans);
+        // ans=a;
+        // Fibonacii(n-1);
+    }
+    // printf("\nLast element:- %d",ans);
+}
