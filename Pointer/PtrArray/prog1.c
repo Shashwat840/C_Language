bd04e979e87d9bdb0276b5f1baef9fa99d909293
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-10 12:42:12 +0530

    ..

---
+++ Pointer/PtrArray/prog1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int no[5],sum =0;
+    int *ptr,i;
+    for(i=0;i<5;i++){
+        printf("\nEnter value of no[%d]:- ",i);
+        scanf("%d",&no[i]);
+    }
+    ptr = no;
+    printf("\nElements\tValue\tAddress\n");
+    for(i=0;i<5;i++){
+        printf("\tno[%d]\t%d\t%d",i,*(ptr+i),ptr);
+        printf("\n");
+        sum = sum + *(ptr+i);
+    }
+    printf("\nSum of all array elements:- %d",sum);
+    return 0;
+}
