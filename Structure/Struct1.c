dd4fbbc9c1f78111ed437798ba116ece7c926cbe
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-16 10:46:03 +0530

    ..

--- Structure/Struct1.c
+++ Structure/Struct1.c
@@ -1,11 +1,21 @@
+//Copying and comparing structure 
 #include<stdio.h>
-struct Student{
-    int id;
-    int rNo;
+#include<string.h>
+struct Class{
+    int number;
     char name[20];
     float marks;
 };
 int main(){
-    struct Student s1[3];
-    
+    struct Class c1 = {1,"Shashwat",100};
+    struct Class c2 = {2,"Shrey",99};
+    struct Class c3;
+    c3 = c2;
+    if((c3.marks == c2.marks)&&(c3.number==c2.number)&&(strcmp(c3.name,c2.name)==0)){
+        printf("\nThe structure varible is same");
+    }
+    else{
+        printf("\nStructure varible are not same");
+    }
+    return 0;
 }
