f9373a03486b8a2502b920822d2beee55892e767
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-23 09:20:31 +0530

    ..

--- Structure/Struct2.c
+++ Structure/Struct2.c
@@ -12,10 +12,10 @@ int main(){
     for(int i = 0;i<3;i++){
         printf("\nEnter id of student:- ");
         scanf("%d",&s->id);
-        printf("\nEnter name of student:- ");
-        scanf("%c ",&s->name);
         printf("\nEnter marks of maths , sci and english:- ");
         scanf("%d %d %d",&s->marks[0],&s->marks[1],&s->marks[2]);
+        printf("\nEnter name of student:- ");
+        scanf("%c ",&s->name);
     }
     for(int i = 0;i<3;i++){
         sum = (s->marks[i] + sum)/3;
