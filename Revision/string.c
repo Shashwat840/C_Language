c8c340d068c4d1e411b504575d31473364497581
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-30 10:43:14 +0530

    ...

--- Revision/string.c
+++ Revision/string.c
@@ -6,10 +6,10 @@ int main(){
     gets(s1);
     printf("\nEnter your string2:- ");
     gets(s2);
-    if(strcmp(s1,s2) > 0){
+    if(strncmp(s1,s2,1) > 0){
         printf("String 1 is greater than String 2");
     }
-    else if(strcmp(s1,s2) < 0){
+    else if(strncmp(s1,s2,1) < 0){
         printf("\nString 2 is greater than String 1");
     }
     else{
