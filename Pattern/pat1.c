cc6c3082ec309b47d6e5b269a0a968a7db91b331
Dave Shashwat <daveshashwat46@gmail.com>
2026-04-30 09:21:53 +0530

    ..

---
+++ Pattern/pat1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int i,j,k,r;
+    printf("\nEnter number of rows:- ");
+    scanf("%d",&r);
+    for(i=1;i<=r;i++){
+        for(k=r;k>=i;k--){
+            printf(" ");
+        }
+        
+        for(j=1;j<=i;j++){
+            printf("* ");
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
