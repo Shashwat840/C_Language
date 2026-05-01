dbbb52afb62d11f6a8c9261dc27f2e9eab14b3a9
Dave Shashwat <daveshashwat46@gmail.com>
2026-05-01 12:03:00 +0530

    ..

--- Revision/Struct.c
+++ Revision/Struct.c
@@ -1,4 +1,24 @@
 #include<stdio.h>
+#include<string.h>
+struct Student {
+    int rno;
+    char name[20];
+    int marks;
+};
 int main(){
-    
+    struct Student s[3];
+    int i;
+    for(i=0;i<=3;i++){
+        printf("\nEnter your rollNo:- ");
+        scanf("%d",&s[i].marks);
+        printf("\nEnter name of student:- ");
+        gets(s[i].name);
+    }
+
+    for(i=0;i<3;i++){
+        printf("\nRoll NO:- %d",s[i].rno);
+        printf("\nName:- %s",s[i].name);
+        printf("\nMarks:- %d",s[i].marks);
+    }
+    return 0;
 }
