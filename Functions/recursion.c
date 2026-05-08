f62ab2b1297798bad509818fd35283ddec49af24
Dave Shashwat <daveshashwat46@gmail.com>
2026-05-08 12:19:36 +0530

    ..

--- Functions/recursion.c
+++ Functions/recursion.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 void Hi(int x);
 int main(){
@@ -9,11 +10,55 @@ int main(){
 }
 void Hi(int x){
     if(x<=0){
-        printf("\nBye...");//Nase class called
+        printf("\nBye...");//Base class called
     }
     else{
         printf("\nHi");
             Hi(x-1);//Recursion called
         
     } 
+}
+*/
+/*
+#include<stdio.h>
+int fact(int n);
+int main(){
+    int n,ans=0;
+    printf("\nEnter number:- ");
+    scanf("%d",&n);
+    ans = fact(n);
+    printf("\nThe factorial of %d :- %d",n,ans);
+    return 0;
+}
+int fact(int n){
+    if (n == 0 || n == 1){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
+*/
+#include<stdio.h>
+int fibonacii(int i);
+int main(){
+    int i,n;
+    printf("\nEnter number:- ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("%d\t",fibonacii(i));
+    }
+    return 0;
+}
+int fibonacii(int i){
+    if (i == 0 ){
+        return 0;
+    }
+    else if (i == 1 ){
+        return 1;
+    }
+    else{
+
+        return fibonacii(i-1) + fibonacii(i-2);
+    }
 }
