d1ce7f906b107ed24fb87d18896f22b60275e2c9
Dave Shashwat <daveshashwat46@gmail.com>
2026-05-08 12:04:35 +0530

    ..

--- Revision/Recussion.c
+++ Revision/Recussion.c
@@ -1,4 +1,19 @@
+void Hi(int x);
 #include<stdio.h>
 int main(){
-    
+    int no;
+    printf("\nEnter number:- ");
+    scanf("%d",&no);
+    Hi(no);
+    return 0;
+}
+void Hi(int x){
+    if(x<0 ){
+        printf("\nBye...");
+        
+    }
+    else{
+        printf("\nHii...");
+        Hi(x-1);
+    }
 }
